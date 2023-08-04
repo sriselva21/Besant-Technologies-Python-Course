9d1d24c0019ed6054dcb80010343f03181b31f01
Sridhar <107684264+sriselva21@users.noreply.github.com>
2023-08-04 12:21:42 +0530

    Create python_dict_exercise.py

---
+++ python_dict_exercise.py
@@ -0,0 +1,417 @@
+# 1.WriteaPythonscripttosort(ascendinganddescending)adictionarybyvalue.
+# 
+# 2.WriteaPythonscripttoaddakeytoadictionary.
+# SampleDictionary:{0:10,1:20}
+# ExpectedResult:{0:10,1:20,2:30}
+# 
+# 3.WriteaPythonscripttoconcatenatethefollowingdictionariestocreateanewone.
+# SampleDictionary:
+# dic1={1:10,2:20}
+# dic2={3:30,4:40}
+# dic3={5:50,6:60}
+# ExpectedResult:{1:10,2:20,3:30,4:40,5:50,6:60}
+# 
+# 4.WriteaPythonscripttocheckwhetheragivenkeyalreadyexistsinadictionary.
+# 
+# 5.WriteaPythonprogramtoiterateoverdictionariesusingforloops.
+# 
+# 6.WriteaPythonscripttogenerateandprintadictionarythatcontainsanumber(between1andn)intheform(x,x*x).
+# SampleDictionary(n=5):
+# ExpectedOutput:{1:1,2:4,3:9,4:16,5:25}
+# 
+# 7.WriteaPythonscripttoprintadictionarywherethekeysarenumbersbetween1and15(bothincluded)andthevaluesarethesquareofthekeys.
+# SampleDictionary
+# {1:1,2:4,3:9,4:16,5:25,6:36,7:49,8:64,9:81,10:100,11:121,12:144,13:169,14:196,15:225}
+# 
+# 8.WriteaPythonscripttomergetwoPythondictionaries.
+# 
+# 9.WriteaPythonprogramtoiterateoverdictionariesusingforloops.
+# 
+# 10.WriteaPythonprogramtosumalltheitemsinadictionary.
+# 
+# 11.WriteaPythonprogramtomultiplyalltheitemsinadictionary.
+# 
+# 12.WriteaPythonprogramtoremoveakeyfromadictionary.
+# 
+# 13.WriteaPythonprogramtomaptwolistsintoadictionary.
+# 
+# 14.WriteaPythonprogramtosortagivendictionarybykey.
+# 
+# 15.WriteaPythonprogramtogetthemaximumandminimumvaluesofadictionary.
+# 
+# 16.WriteaPythonprogramtogetadictionaryfromanobject'sfields.
+# 
+# 17.WriteaPythonprogramtoremoveduplicatesfromthedictionary.
+# 
+# 18.WriteaPythonprogramtocheckifadictionaryisemptyornot.
+# 
+# 19.WriteaPythonprogramtocombinetwodictionarybyaddingvaluesforcommonkeys.
+# d1={'a':100,'b':200,'c':300}
+# d2={'a':300,'b':200,'d':400}
+# Sampleoutput:Counter({'a':400,'b':400,'d':400,'c':300})
+# 
+# 20.WriteaPythonprogramtoprintalldistinctvaluesinadictionary.
+# SampleData:[{"V":"S001"},{"V":"S002"},{"VI":"S001"},{"VI":"S005"},{"VII":"S005"},{"V":"S009"},{"VIII":"S007"}]
+# ExpectedOutput:UniqueValues:{'S005','S002','S007','S001','S009'}
+# 
+# 21.WriteaPythonprogramtocreateanddisplayallcombinationsofletters,selectingeachletterfromadifferentkeyinadictionary.
+# Sampledata:{'1':['a','b'],'2':['c','d']}
+# ExpectedOutput:
+# ac
+# ad
+# bc
+# bd
+# 
+# 22.WriteaPythonprogramtofindthehighest3valuesofcorrespondingkeysinadictionary.
+# 
+# 23.WriteaPythonprogramtocombinevaluesinalistofdictionaries.
+# Sampledata:[{'item':'item1','amount':400},{'item':'item2','amount':300},{'item':'item1','amount':750}]
+# ExpectedOutput:Counter({'item1':1150,'item2':300})
+# 
+# 24.WriteaPythonprogramtocreateadictionaryfromastring.
+# Note:Trackthecountofthelettersfromthestring.
+# Samplestring:'w3resource'
+# Expectedoutput:{'w':1,'3':1,'r':2,'e':2,'s':1,'o':1,'u':1,'c':1}
+# 
+# 25.WriteaPythonprogramtoprintadictionaryintableformat.
+# 
+# 26.WriteaPythonprogramtocountthevaluesassociatedwithakeyinadictionary.
+# ExpectedOutput:
+# 6
+# 2
+# 
+# 27.WriteaPythonprogramtoconvertalistintoanesteddictionaryofkeys.
+# 
+# 28.WriteaPythonprogramtosortalistalphabeticallyinadictionary.
+# 
+# 29.WriteaPythonprogramtoremovespacesfromdictionarykeys.
+# 
+# 30.WriteaPythonprogramtogetthetopthreeitemsinashop.
+# Sampledata:{'item1':45.50,'item2':35,'item3':41.30,'item4':55,'item5':24}
+# ExpectedOutput:
+# item455
+# item145.5
+# item341.3
+# 
+# 31.WriteaPythonprogramtogetthekey,valueanditeminadictionary.
+# 
+# 32.WriteaPythonprogramtoprintadictionarylinebyline.
+# 
+# 33.WriteaPythonprogramtocheckifmultiplekeysexistinadictionary.
+# 
+# 34.WriteaPythonprogramtocountthenumberofitemsinadictionaryvaluethatisalist.
+# 
+# 35.WriteaPythonprogramtosortCounterbyvalue.
+# Sampledata:{'Math':81,'Physics':83,'Chemistry':87}
+# Expecteddata:[('Chemistry',87),('Physics',83),('Math',81)]
+# 
+# 36.WriteaPythonprogramtocreateadictionaryfromtwolistswithoutlosingduplicatevalues.
+# Samplelists:['Class-V','Class-VI','Class-VII','Class-VIII'],[1,2,2,3]
+# ExpectedOutput:defaultdict(<class'set'>,{'Class-V':{1},'Class-VI':{2},'Class-VII':{2},'Class-VIII':{3}})
+# 
+# 37.WriteaPythonprogramtoreplacedictionaryvalueswiththeirsums.
+# 
+# 38.WriteaPythonprogramtomatchkeyvaluesintwodictionaries.
+# Sampledictionary:{'key1':1,'key2':3,'key3':2},{'key1':1,'key2':2}
+# Expectedoutput:key1:1ispresentinbothxandy
+# 
+# 39.WriteaPythonprogramtostoredictionarydatainaJSONfile.
+# Originaldictionary:
+# {'students':[{'firstName':'Nikki','lastName':'Roysden'},{'firstName':'Mervin','lastName':'Friedland'},{'firstName':'Aron','lastName':'Wilkins'}],'teachers':[{'firstName':'Amberly','lastName':'Calico'},{'firstName':'Regine','lastName':'Agtarap'}]}
+# <class'dict'>
+# Jsonfiletodictionary:
+# {'students':[{'firstName':'Nikki','lastName':'Roysden'},{'firstName':'Mervin','lastName':'Friedland'},{'firstName':'Aron','lastName':'Wilkins'}],'teachers':[{'firstName':'Amberly','lastName':'Calico'},{'firstName':'Regine','lastName':'Agtarap'}]}
+# 
+# 40.WriteaPythonprogramtocreateadictionaryofkeysx,y,andzwhereeachkeyhasasvaluealistfrom11-20,21-30,and31-40respectively.Accessthefifthvalueofeachkeyfromthedictionary.
+# {'x':[11,12,13,14,15,16,17,18,19],
+# 'y':[21,22,23,24,25,26,27,28,29],
+# 'z':[31,32,33,34,35,36,37,38,39]}
+# 15
+# 25
+# 35
+# xhasvalue[11,12,13,14,15,16,17,18,19]
+# yhasvalue[21,22,23,24,25,26,27,28,29]
+# zhasvalue[31,32,33,34,35,36,37,38,39]
+# 
+# 41.WriteaPythonprogramtodropemptyitemsfromagivendictionary.
+# OriginalDictionary:
+# {'c1':'Red','c2':'Green','c3':None}
+# NewDictionaryafterdroppingemptyitems:
+# {'c1':'Red','c2':'Green'}
+# 
+# 42.WriteaPythonprogramtofilteradictionarybasedonvalues.
+# OriginalDictionary:
+# {'CierraVega':175,'AldenCantrell':180,'KierraGentry':165,'PierreCox':190}
+# Marksgreaterthan170:
+# {'CierraVega':175,'AldenCantrell':180,'PierreCox':190}
+# 
+# 43.WriteaPythonprogramtoconvertmorethanonelisttoanesteddictionary.
+# Originalstrings:
+# ['S001','S002','S003','S004']
+# ['AdinaPark','LeytonMarsh','DuncanBoyle','SaimRichards']
+# [85,98,89,92]
+# Nesteddictionary:
+# [{'S001':{'AdinaPark':85}},{'S002':{'LeytonMarsh':98}},{'S003':{'DuncanBoyle':89}},{'S004':{'SaimRichards':92}}]
+# 
+# 44.WriteaPythonprogramtofiltertheheightandwidthofstudents,whicharestoredinadictionary.
+# OriginalDictionary:
+# {'CierraVega':(6.2,70),'AldenCantrell':(5.9,65),'KierraGentry':(6.0,68),'PierreCox':(5.8,66)}
+# Height>6ftandWeight>70kg:
+# {'CierraVega':(6.2,70)}
+# 
+# 45.WriteaPythonprogramtoverifythatallvaluesinadictionaryarethesame.
+# OriginalDictionary:
+# {'CierraVega':12,'AldenCantrell':12,'KierraGentry':12,'PierreCox':12}
+# Checkallare12inthedictionary.
+# True
+# Checkallare10inthedictionary.
+# False
+# 
+# 46.WriteaPythonprogramtocreateadictionarygroupingasequenceofkey-valuepairsintoadictionaryoflists.
+# Originallist:
+# [('yellow',1),('blue',2),('yellow',3),('blue',4),('red',1)]
+# Groupingasequenceofkey-valuepairsintoadictionaryoflists:
+# {'yellow':[1,3],'blue':[2,4],'red':[1]}
+# 
+# 47.WriteaPythonprogramtosplitagivendictionaryoflistsintolistsofdictionaries.
+# Originaldictionaryoflists:
+# {'Science':[88,89,62,95],'Language':[77,78,84,80]}
+# Splitsaiddictionaryoflistsintolistofdictionaries:
+# [{'Science':88,'Language':77},{'Science':89,'Language':78},{'Science':62,'Language':84},{'Science':95,'Language':80}]
+# 
+# 48.WriteaPythonprogramtoremoveaspecifieddictionaryfromagivenlist.
+# Originallistofdictionary:
+# [{'id':'FF0000','color':'Red'},{'id':'800000','color':'Maroon'},{'id':'FFFF00','color':'Yellow'},{'id':'808000','color':'Olive'}]
+# RemoveidFF0000fromthesaidlistofdictionary:
+# [{'id':'800000','color':'Maroon'},{'id':'FFFF00','color':'Yellow'},{'id':'808000','color':'Olive'}]
+# 
+# 49.WriteaPythonprogramtoconvertstringvaluesofagivendictionaryintointeger/floatdatatypes.
+# Originallist:
+# [{'x':'10','y':'20','z':'30'},{'p':'40','q':'50','r':'60'}]
+# Stringvaluesofagivendictionary,intointegertypes:
+# [{'x':10,'y':20,'z':30},{'p':40,'q':50,'r':60}]
+# Originallist:
+# [{'x':'10.12','y':'20.23','z':'30'},{'p':'40.00','q':'50.19','r':'60.99'}]
+# Stringvaluesofagivendictionary,intofloattypes:
+# [{'x':10.12,'y':20.23,'z':30.0},{'p':40.0,'q':50.19,'r':60.99}]
+# 
+# 50.APythondictionarycontainsListasavalue.WriteaPythonprogramtoclearthelistvaluesinthesaiddictionary.
+# OriginalDictionary:
+# {'C1':[10,20,30],'C2':[20,30,40],'C3':[12,34]}
+# Clearthelistvaluesinthesaiddictionary:
+# {'C1':[],'C2':[],'C3':[]}
+# 
+# 51.APythonDictionarycontainsListasavalue.WriteaPythonprogramtoupdatethelistvaluesinthesaiddictionary.
+# OriginalDictionary:
+# {'Math':[88,89,90],'Physics':[92,94,89],'Chemistry':[90,87,93]}
+# Updatethelistvaluesofthesaiddictionary:
+# {'Math':[89,90,91],'Physics':[90,92,87],'Chemistry':[90,87,93]}
+# 
+# 52.WriteaPythonprogramtoextractalistofvaluesfromagivenlistofdictionaries.
+# OriginalDictionary:
+# [{'Math':90,'Science':92},{'Math':89,'Science':94},{'Math':92,'Science':88}]
+# Extractalistofvaluesfromsaidlistofdictionarieswheresubject=Science
+# [92,94,88]
+# OriginalDictionary:
+# [{'Math':90,'Science':92},{'Math':89,'Science':94},{'Math':92,'Science':88}]
+# Extractalistofvaluesfromsaidlistofdictionarieswheresubject=Math
+# [90,89,92]
+# 
+# 53.WriteaPythonprogramtofindthelengthofadictionaryofvalues.
+# OriginalDictionary:
+# {1:'red',2:'green',3:'black',4:'white',5:'black'}
+# Lengthofdictionaryvalues:
+# {'red':3,'green':5,'black':5,'white':5}
+# OriginalDictionary:
+# {'1':'AustinLittle','2':'NatashaHoward','3':'AlfredMullins','4':'JamieRowe'}
+# Lengthofdictionaryvalues:
+# {'AustinLittle':13,'NatashaHoward':14,'AlfredMullins':14,'JamieRowe':10}
+# 
+# 54.WriteaPythonprogramtogetthedepthofadictionary.
+# ExpectedOutput:
+# 4
+# 
+# 55.WriteaPythonprogramtoaccessdictionarykey'selementbyindex.
+# ExpectedOutput:
+# physics
+# math
+# chemistry
+# 
+# 56.WriteaPythonprogramtoconvertadictionaryintoalistoflists.
+# OriginalDictionary:
+# {1:'red',2:'green',3:'black',4:'white',5:'black'}
+# Convertthesaiddictionaryintoalistoflists:
+# [[1,'red'],[2,'green'],[3,'black'],[4,'white'],[5,'black']]
+# OriginalDictionary:
+# {'1':'AustinLittle','2':'NatashaHoward','3':'AlfredMullins','4':'JamieRowe'}
+# Convertthesaiddictionaryintoalistoflists:
+# [['1','AustinLittle'],['2','NatashaHoward'],['3','AlfredMullins'],['4','JamieRowe']]
+# 
+# 57.WriteaPythonprogramtofilterevennumbersfromadictionaryofvalues.
+# OriginalDictionary:
+# {'V':[1,4,6,10],'VI':[1,4,12],'VII':[1,3,8]}
+# Filterevennumbersfromsaiddictionaryvalues:
+# {'V':[4,6,10],'VI':[4,12],'VII':[8]}
+# OriginalDictionary:
+# {'V':[1,3,5],'VI':[1,5],'VII':[2,7,9]}
+# Filterevennumbersfromsaiddictionaryvalues:
+# {'V':[],'VI':[],'VII':[2]}
+# 
+# 58.WriteaPythonprogramtogetallcombinationsofkey-valuepairsinagivendictionary.
+# OriginalDictionary:
+# {'V':[1,4,6,10],'VI':[1,4,12],'VII':[1,3,8]}
+# Combinationsofkey-valuepairsofthesaiddictionary:
+# [{'V':[1,4,6,10],'VI':[1,4,12]},{'V':[1,4,6,10],'VII':[1,3,8]},{'VI':[1,4,12],'VII':[1,3,8]}]
+# OriginalDictionary:
+# {'V':[1,3,5],'VI':[1,5]}
+# Combinationsofkey-valuepairsofthesaiddictionary:
+# [{'V':[1,3,5],'VI':[1,5]}]
+# 
+# 59.WriteaPythonprogramtofindthespecifiednumberofmaximumvaluesinagivendictionary.
+# OriginalDictionary:
+# {'a':5,'b':14,'c':32,'d':35,'e':24,'f':100,'g':57,'h':8,'i':100}
+# 1maximumvalue(s)inthesaiddictionary:
+# ['f']
+# 2maximumvalue(s)inthesaiddictionary:
+# ['f','i']
+# 5maximumvalue(s)inthesaiddictionary:
+# ['f','i','g','d','c']
+# 
+# 60.WriteaPythonprogramtofindtheshortestlistofvaluesforthekeysinagivendictionary.
+# OriginalDictionary:{'V':[10,12],'VI':[10],'VII':[10,20,30,40],'VIII':[20],'IX':[10,30,50,70],'X':[80]}Shortestlistofvalueswiththekeysofthesaiddictionary:['VI','VIII','X']
+# 
+# 61.WriteaPythonprogramtocountthefrequencyofadictionary.
+# OriginalDictionary:
+# {'V':10,'VI':10,'VII':40,'VIII':20,'IX':70,'X':80,'XI':40,'XII':20}
+# Countthefrequencyofthesaiddictionary:
+# Counter({10:2,40:2,20:2,70:1,80:1})
+# 
+# 62.WriteaPythonprogramtoextractvaluesfromagivendictionaryandcreatealistoflistsfromthosevalues.
+# OriginalDictionary:
+# [{'student_id':1,'name':'JeanCastro','class':'V'},{'student_id':2,'name':'LulaPowell','class':'V'},{'student_id':3,'name':'BrianHowell','class':'VI'},{'student_id':4,'name':'LynneFoster','class':'VI'},{'student_id':5,'name':'ZacharySimon','class':'VII'}]
+# Extractvaluesfromthesaiddictionarieandcreatealistoflistsusingthosevalues:
+# [[1,'JeanCastro','V'],[2,'LulaPowell','V'],[3,'BrianHowell','VI'],[4,'LynneFoster','VI'],[5,'ZacharySimon','VII']]
+# [[1,'JeanCastro'],[2,'LulaPowell'],[3,'BrianHowell'],[4,'LynneFoster'],[5,'ZacharySimon']]
+# [['JeanCastro','V'],['LulaPowell','V'],['BrianHowell','VI'],['LynneFoster','VI'],['ZacharySimon','VII']]
+# 
+# 63.WriteaPythonprogramtoconvertagivenlistofliststoadictionary.
+# Originallistoflists:
+# [[1,'JeanCastro','V'],[2,'LulaPowell','V'],[3,'BrianHowell','VI'],[4,'LynneFoster','VI'],[5,'ZacharySimon','VII']]
+# Convertthesaidlistofliststoadictionary:
+# {1:['JeanCastro','V'],2:['LulaPowell','V'],3:['BrianHowell','VI'],4:['LynneFoster','VI'],5:['ZacharySimon','VII']}
+# 
+# 64.WriteaPythonprogramthatcreateskey-valuelistpairingswithinadictionary.
+# Originaldictionary:
+# {1:['JeanCastro'],2:['LulaPowell'],3:['BrianHowell'],4:['LynneFoster'],5:['ZacharySimon']}
+# Akey-valuelistpairingsofthesaiddictionary:
+# [{1:'JeanCastro',2:'LulaPowell',3:'BrianHowell',4:'LynneFoster',5:'ZacharySimon'}]
+# 
+# 65.WriteaPythonprogramtogetthetotallengthofallvaluesinagivendictionarywithstringvalues.
+# Originaldictionary:
+# {'FF0000':'Red','800000':'Maroon','FFFF00':'Yellow','808000':'Olive'}
+# Totallengthofallvaluesofthesaiddictionarywithstringvalues:
+# 20
+# 
+# 66.WriteaPythonprogramtocheckifaspecifickeyandavalueexistinadictionary.
+# Originaldictionary:
+# [{'student_id':1,'name':'JeanCastro','class':'V'},{'student_id':2,'name':'LulaPowell','class':'V'},{'student_id':3,'name':'BrianHowell','class':'VI'},{'student_id':4,'name':'LynneFoster','class':'VI'},{'student_id':5,'name':'ZacharySimon','class':'VII'}]
+# CheckifaspecificKeyandavalueexistinthesaiddictionary:
+# True
+# True
+# True
+# False
+# False
+# False
+# 
+# 67.WriteaPythonprogramtoinvertagivendictionarywithnon-uniquehashablevalues.
+# SampleOutput:
+# {8:['OraMckinney','MathewGilbert'],7:['TheodoreHollandl','MaeFleming','IvanLittle']}
+# 
+# 68.WriteaPythonprogramtocombinetwoormoredictionaries,creatingalistofvaluesforeachkey.
+# SampleOutput:
+# Originaldictionaries:
+# {'w':50,'x':100,'y':'Green','z':400}
+# {'x':300,'y':'Red','z':600}
+# Combineddictionaries,creatingalistofvaluesforeachkey:
+# {'w':[50],'x':[100,300],'y':['Green','Red'],'z':[400,600]}
+# 
+# 69.WriteaPythonprogramtogrouptheelementsofagivenlistbasedonthegivenfunction.
+# SampleOutput:
+# Originallist&function:
+# [7,23,3.2,3.3,8.4]Functionname:floor:
+# Grouptheelementsofthesaidlistbasedonthegivenfunction:
+# {7:[7],23:[23],3:[3.2,3.3],8:[8.4]}
+# Originallist&function:
+# ['Red','Green','Black','White','Pink']Functionname:len:
+# Grouptheelementsofthesaidlistbasedonthegivenfunction:
+# {3:['Red'],5:['Green','Black','White'],4:['Pink']}
+# 
+# 70.WriteaPythonprogramtomapthevaluesofagivenlisttoadictionaryusingafunction,wherethekey-valuepairsconsistoftheoriginalvalueasthekeyandtheresultofthefunctionasthevalue.
+# SampleOutput:
+# {1:1,2:4,3:9,4:16}
+# 
+# 71.WriteaPythonprogramtoretrievethevalueofthenestedkeyindicatedbythegivenselectorlistfromadictionaryorlist.
+# SampleOutput:
+# Russell
+# 2
+# 
+# 72.WriteaPythonprogramtoinvertadictionarywithuniquehashablevalues.
+# SampleOutput:
+# {10:'Theodore',11:'Mathew',9:'Roxanne'}
+# 
+# 73.WriteaPythonprogramtoconvertalistofdictionariesintoalistofvaluescorrespondingtothespecifiedkey.
+# SampleOutput:
+# Originallistofdictionaries:
+# [{'name':'Theodore','age':18},{'name':'Mathew','age':22},{'name':'Roxanne','age':20},{'name':'David','age':18}]
+# Convertalistofdictionariesintoalistofvaluescorrespondingtothespecifiedkey:
+# [18,22,20,18]
+# 
+# 74.WriteaPythonprogramtocreateadictionarywiththesamekeysasthegivendictionaryandvaluesgeneratedbyrunningthegivenfunctionforeachvalue.
+# SampleOutput:
+# Originaldictionaryelements:
+# {'Theodore':{'user':'Theodore','age':45},'Roxanne':{'user':'Roxanne','age':15},'Mathew':{'user':'Mathew','age':21}}
+# Dictionarywiththesamekeys:
+# {'Theodore':45,'Roxanne':15,'Mathew':21}
+# 
+# 75.WriteaPythonprogramtofindallkeysinadictionarythathavethegivenvalue.
+# SampleOutput:
+# Originaldictionaryelements:
+# {'Theodore':19,'Roxanne':20,'Mathew':21,'Betty':20}
+# Findallkeysinthesaiddictionarythathavethespecifiedvalue:
+# ['Roxanne','Betty']
+# 
+# 76.WriteaPythonprogramtocombinetwolistsintoadictionary.Theelementsofthefirstoneserveaskeysandtheelementsofthesecondoneserveasvalues.Eachiteminthefirstlistmustbeuniqueandhashable.
+# SampleOutput:
+# Originallists:
+# ['a','b','c','d','e','f']
+# [1,2,3,4,5]
+# Combinethevaluesofthesaidtwolistsintoadictionary:
+# {'a':1,'b':2,'c':3,'d':4,'e':5}
+# 
+# 77.WriteaPythonprogramtotransformadictionaryintoalistoftuples.
+# SampleOutput:
+# OriginalDictionary:
+# {'Red':1,'Green':3,'White':5,'Black':2,'Pink':4}
+# Convertthesaiddictionarytoalistoftuples:
+# [('Red',1),('Green',3),('White',5),('Black',2),('Pink',4)]
+# 
+# 78.WriteaPythonprogramtocreateaflatlistofallthekeysinaflatdictionary.
+# SampleOutput:
+# Originaldictionaryelements:
+# {'Theodore':19,'Roxanne':20,'Mathew':21,'Betty':20}
+# Createaflatlistofallthekeysofthesaidflatdictionary:
+# ['Theodore','Roxanne','Mathew','Betty']
+# 
+# 79.WriteaPythonprogramtocreateaflatlistofallthevaluesinaflatdictionary.
+# SampleOutput:
+# Originaldictionaryelements:
+# {'Theodore':19,'Roxanne':20,'Mathew':21,'Betty':20}
+# Createaflatlistofallthevaluesofthesaidflatdictionary:
+# [19,20,21,20]
+# 
+# 80.WriteaPythonprogramtofindthekeyofthemaximumvalueinadictionary.
+# SampleOutput:
+# Originaldictionaryelements:
+# {'Theodore':19,'Roxanne':22,'Mathew':21,'Betty':20}
+# Findsthekeyofthemaximumandminimumvalueofthesaiddictionary:
+# ('Roxanne','Theodore')
