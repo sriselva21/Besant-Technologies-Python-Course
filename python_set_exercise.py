504ae692e9c090b3287978e53abd169501d2aecd
Sridhar <107684264+sriselva21@users.noreply.github.com>
2023-08-04 12:15:46 +0530

    Create python_set_exercise.py

---
+++ python_set_exercise.py
@@ -0,0 +1,59 @@
+# 1. Write a Python program to create a set.
+
+# 2. Write a Python program to iterate over sets.
+
+# 3. Write a Python program to add member(s) to a set.
+
+# 4. Write a Python program to remove item(s) from a given set.
+
+# 5. Write a Python program to remove an item from a set if it is present in the set.
+
+# 6. Write a Python program to create an intersection of sets.
+
+# 7. Write a Python program to create a union of sets.
+
+# 8. Write a Python program to create set difference.
+
+# 9. Write a Python program to create a symmetric difference.
+
+# 10. Write a Python program to check if a set is a subset of another set.
+
+# 11. Write a Python program to create a shallow copy of sets.
+
+# 12. Write a Python program to remove all elements from a given set.
+
+# 13. Write a Python program that uses frozensets.
+
+# 14. Write a Python program to find the maximum and minimum values in a set.
+
+# 15. Write a Python program to find the length of a set.
+
+# 16. Write a Python program to check if a given value is present in a set or not.
+
+# 17. Write a Python program to check if two given sets have no elements in common.
+
+# 18. Write a Python program to check if a given set is a superset of itself and a superset of another given set.
+
+# 19. Write a Python program to find elements in a given set that are not in another set.
+
+# 20. Write a Python program to remove the intersection of a second set with a first set.
+
+# 21. Write a Python program to find all the unique words and count the frequency of occurrence from a given list of strings. Use Python set data type.
+
+# 22. Write a Python program that finds all pairs of elements in a list whose sum is equal to a given value.
+
+# 23. Write a Python program to find the longest common prefix of all strings. Use the Python set.
+
+# 24. Write a Python program to find the two numbers whose product is maximum among all the pairs in a given list of numbers. Use the Python set.
+
+# 25. Given two sets of numbers, write a Python program to find the missing numbers in the second set as compared to the first and vice versa. Use the Python set.
+
+# 26. Write a Python program to find all the anagrams and group them together from a given list of strings. Use the Python data type.
+
+# 27. Write a Python program to find all the anagrams in a given list of strings and then group them together. Use the Python data type.
+
+# 28. Write a Python program to find all the unique combinations of 3 numbers from a given list of numbers, adding up to a target number.
+
+# 29. Write a Python program to find the third largest number from a given list of numbers.Use the Python set data type.
+
+# 30. Write a Python program to remove all duplicates from a given list of strings and return a list of unique strings. Use the Python set data type.
